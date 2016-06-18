d6eadccf9e6119dd2a5af06f11662917b5139091
Autumn Marsh <Autumn Marsh>
2016-06-18 15:27:39 -0700

    Commiting homework files

---
+++ js/script.js
@@ -0,0 +1,165 @@
+// Assignment 1, Part 2
+// Autumn Marsh
+// FEWD2 
+
+
+// step 1 
+/*
+var someMonth;
+function theMonth();        // function to return current month
+currentMonth                // a constant
+var summerMonth[];          // an array of summer months
+myLibrary();                // a function 
+
+
+// step 2 
+45                          // Numeric literal expression
+'Fuzzy bunnies'             // String literal expression
+true                        // Boolean literal expression
+null                        // Null literal expression
+
+
+// step 3: 2 examples of complex / variable expressions
+var myExpression = 9 * (5 * 3);
+var mySecondExp = "myExpression" + " is the answer";
+
+
+// step 4 
+var strFirstName, strLastName, txtAddress, city, resideState, zip, intYourAge, refSource, blnContact;
+
+
+// step 5
+strFirstName = "Wanda";
+blnContact = true, intYourAge = 45;
+var customerID = "Wanda" + " " + "Winston";
+
+
+// step 6
+var yearsToRetire = 30;
+    console.log("You have " + yearsToRetire + " years until retirement");
+
+var mustStudy = true;
+    console.log("It is " + mustStudy + " that you must work your bleep off in FEWD2");
+
+ var myBool = true;
+    console.log(3 + myBool);
+
+
+// step 7
+var candy = "chocolate";
+    console.log("My candy of choice is " + type + candy + ".");
+var type = "dark";
+
+
+// step 8
+No, it is not valid. I would fix it by escaping the quotation marks and apostrophe that need to render:
+
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+    console.log(someString);
+
+
+// step 9
+var x = null;
+    console.log(x);
+
+var y;
+    console.log(y);
+
+
+// step 10
+var myString = "This is a string";
+    console.log(typeof(myString));
+
+var x = 10;
+    console.log(typeof(x));
+
+var isDone = true;
+    console.log(typeof(isDone));
+
+console.log(typeof(x));             
+
+
+// step 11
+var firstName = "Zak";
+var lastName = "Ruvalcaba";
+var fullName = (firstName + " " + lastName);
+    //alert("Hello " + fullName + ", welcome to the JavaScript class!");
+// step 12
+var name = "Autumn Marsh";
+    //alert("Hello " + name + ", welcome to the JavaScript class!");
+// step 13
+var course = "JavaScript";
+    //alert("Hello " + name + ", welcome to the " + course + " class!");
+// step 14
+    //alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+// step 15
+var name = prompt("What is your full name?", "First name, last name");
+    console.log(name);
+// step 16
+var course = prompt("What class are you taking?", "Enter class name");
+    console.log(course);
+    
+
+// step 17
+var x = 10;
+var y = 20;
+    console.log(x + y);
+
+// step 18
+var x = 20;
+    console.log(x += x);
+    
+// step 19
+var x = 20;
+    console.log(x *= 5);
+
+// step 20
+x = 20 % 3;
+x /= 1;
+    console.log(x);
+
+// step 21
+var x = 7;
+var y = true;
+    console.log(x < 4 || y);
+
+// step 22
+var x = "99 bottles of beer";
+var y = 1;
+    console.log(x > y);
+
+// step 23
+var widget = new Object();
+    console.log(typeof(widget));
+
+// step 24
+var widget = new Object();
+    alert(widget instanceof Object);
+
+// step 25
+var widget = new Object();
+    alert(widget instanceof String);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
